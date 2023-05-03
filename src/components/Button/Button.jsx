efa9afb237262d40bb6f7f6cd9610ebc7cc2ce79
KrlosPK <centille095@gmail.com>
2023-05-02 20:45:57 -0500

    Change button styles using CSS Modules

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import styles from './Button.module.css'
 
 const Button = ({ text, isBackground }) => {
   return (
-    <button tabIndex={-1} className={isBackground && styles.bg}>
+    <button tabIndex={-1} className={`${styles.button} ${isBackground && styles.bg}`}>
       {text}
     </button>
   )
